6c4b7ef6c6042749623cbcc90243b28eceb3c314
Abdulquddus Ajibade <ajiscomorac@gmail.com>
2022-07-25 23:08:24 +0100

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,6 +1,6 @@
 import nltk
-nltk.download('punkt')
-#   nltk.download()
+#nltk.download('punkt')
+nltk.download()
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
